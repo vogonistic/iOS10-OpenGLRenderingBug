9369beace7c9c0339f98a4e5964aa264854eb16b
Kent Karlsson <vogonistic@gmail.com>
2017-01-06 11:43:49 -0800

    Changed demo to use GLKView

--- TestVideoTextureBug/ViewController.swift
+++ TestVideoTextureBug/ViewController.swift
@@ -10,25 +10,21 @@ import UIKit
 import SceneKit
 import SpriteKit
 
-class ViewController: UIViewController {
-	@IBOutlet weak var scnView: SCNView!
+let kShowVideo = true
+
+class ViewController: GLKViewController {
+    var renderer: SCNRenderer!
 
 	override func viewDidLoad() {
+        print("\(#function)")
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-		scnView.showsStatistics = true
-		scnView.allowsCameraControl = true
 
-		let scnScene = SCNScene()
-		scnView.scene = scnScene
-
-		print("scnView renderingAPI is metal", scnView.renderingAPI == SCNRenderingAPI.metal)
-		print("scnView renderingAPI is opengl", scnView.renderingAPI == SCNRenderingAPI.openGLES2)
+        let scnScene = SCNScene()
 
 		// setup SceneKit scene
 		let cameraNode = SCNNode()
 		cameraNode.camera = SCNCamera()
-		cameraNode.position = SCNVector3(x: 0.0, y: 0.0, z: 25.0)
+		cameraNode.position = SCNVector3(x: 0.0, y: 0.0, z: 20.0)
 		scnScene.rootNode.addChildNode(cameraNode)
         scnScene.background.contents = UIColor.lightGray
 
@@ -37,26 +33,45 @@ class ViewController: UIViewController {
 		scnScene.rootNode.addChildNode(cubeNode)
 
 		// setup SpriteKit Scene
+        let size = CGSize(width: 480, height: 480)
 		let skScene = SKScene()
-		skScene.backgroundColor = UIColor.green
-		skScene.size = CGSize(width: 480, height: 360)
+		skScene.backgroundColor = UIColor.blue
+		skScene.size = size
 
-        let videoNode = SKVideoNode(url: URL(string: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8")!)
-        videoNode.yScale = -1
-        videoNode.size = CGSize(width: 480, height: 360)
-        videoNode.position = CGPoint(x: 240, y: 180)
-        skScene.addChild(videoNode)
+        if kShowVideo {
+            let videoNode = SKVideoNode(url: URL(string: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8")!)
+            videoNode.yScale = -1
+            videoNode.xScale = -1
+            videoNode.size = size
+            videoNode.position = CGPoint(x: size.width/2.0, y: size.height/2.0)
+            skScene.addChild(videoNode)
+
+            videoNode.play()
+        } else {
+            let skNode = SKShapeNode(rect: CGRect(x: 0.0, y: 0.0, width: 10.0, height: 10.0))
+            skNode.fillColor = UIColor.green
+            skNode.position = CGPoint(x: 5.0, y: 5.0)
+            skScene.addChild(skNode)
+        }
 
 		cubeNode.geometry?.firstMaterial?.diffuse.contents = skScene
+//        cubeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+        cubeNode.geometry?.firstMaterial?.isDoubleSided = true
 
-        let cubeRotation = CABasicAnimation(keyPath: "transform")
-        cubeRotation.toValue = NSValue(scnMatrix4: SCNMatrix4Rotate(cubeNode.transform, Float.pi, 1, 1, 0))
-        cubeRotation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-        cubeRotation.repeatCount = Float.greatestFiniteMagnitude
-        cubeRotation.duration = 5.0
-        cubeNode.addAnimation(cubeRotation, forKey: "RotateCube")
+        let context = EAGLContext(api: .openGLES2)!
+        EAGLContext.setCurrent(context)
+        (view as? GLKView)?.context = context
 
-        videoNode.play()
-	}
+        renderer = SCNRenderer(context: context, options: nil)
+        renderer.scene = scnScene
+        renderer.showsStatistics = true
+    }
+
+    override func glkView(_ view: GLKView, drawIn rect: CGRect) {
+        glClearColor(1.0, 1.0, 1.0, 1.0)
+        glClear(GLbitfield(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT))
+
+        renderer.render(atTime: 0.0)
+    }
 }
 
